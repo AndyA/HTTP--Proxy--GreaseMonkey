55ffa1f04a7c8dcdf4f66b2531eb168451f7d076
Andy Armstrong <andy@hexten.net>
2007-12-15 21:33:13 +0000

    Added

---
+++ lib/HTTP/Proxy/GreaseMonkey.pm
@@ -0,0 +1,62 @@
+package HTTP::Proxy::GreaseMonkey;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+HTTP::Proxy::GreaseMonkey - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes HTTP::Proxy::GreaseMonkey version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use HTTP::Proxy::GreaseMonkey;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+HTTP::Proxy::GreaseMonkey requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-http-proxy-greasemonkey@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2007, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'HTTP::Proxy::GreaseMonkey' );
+}
+
+diag( "Testing HTTP::Proxy::GreaseMonkey $HTTP::Proxy::GreaseMonkey::VERSION" );
